a261d0419a81bdc608f4f9efb65ad5c7f1d68d8f
wildsong550 <wildsong550@gmail.com>
2024-08-15 21:15:56 -0400

    Add files via upload

---
+++ PostgreSQL.sql
@@ -0,0 +1,13 @@
+create table usuario(
+  id_usuario INTEGER primary key not NULL,
+  nome VARCHAR(100)
+  idade INTEGER
+  );
+  
+create table aluno(
+  id_aluno INTEGER PRIMARY KEY NOT NULL,
+  nome VARCHAR(250)
+  idade INTEGER,
+  ano  INTEGER,
+  periodo INTEGER,
+  )
